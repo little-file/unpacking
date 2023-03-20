d9c6af59c8122f66a486607bc7302619c817ed21
little-file <fakelittleaqqff@protonmail.com>
2023-03-21 02:08:44 +0300

    we changed the name and added the zip

--- Unpacking.py
+++ Unpacking.py
@@ -1,23 +1,30 @@
 #power by https://github.com/little-file/
 #version 1
 import tarfile
+import zipfile
 
 def tar():
     print(".tar")
-    tar_path = input("example /path/to/tarfile.tar:")
+    tar_path = input("example /path/to/tarfile.tar: ")
     with tarfile.open(tar_path, "r") as tar:
         tar.extractall()
 def tar_gz():
     print(".tar.gz")
-    tar_path = input("example /path/to/tarfile.tar:")
+    tar_path = input("example /path/to/tarfile.tar: ")
     with tarfile.open(tar_path, "r:gz") as tar:
         tar.extractall()
+def zipfile():
+    print(".zip")
+    zip_path = input("example /path/to/zipfile.zip:")
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall()
 
 while True:
     print("""
     [1]tarfile.tar
     [2]tarfile.tar.gz
-    [3]quite
+    [3]zipfile.zip
+    [4]quite
     """)
     choice = int(input("Enter your choice: "))
     if choice == 1:
@@ -25,6 +32,8 @@ while True:
     elif choice == 2:
         tar_gz()
     elif choice == 3:
-        break 
+        zipfile() 
+    elif choice == 4:
+        break
     else:
         print("invalid choice. Please try again.")
