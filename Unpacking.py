ad7565a95033a57eeea3d2d6728ee290e47cf768
little-file <fakelittleaqqff@protonmail.com>
2023-03-27 00:33:28 +0300

    bz2

--- Unpacking.py
+++ Unpacking.py
@@ -18,13 +18,19 @@ def zip():
     zip_path = input("example /path/to/zipfile.zip:")
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall()
+def tar_bz2():
+    print(".tar.bz2")
+    tar_path = input("example /path/to/tarfile.tar: ")
+    with tarfile.open(tar_path, "r:bz2") as tar:
+        tar.extractall()
 
 while True:
     print("""
     [1]tarfile.tar
     [2]tarfile.tar.gz
     [3]zipfile.zip
-    [4]quite
+    [4]tarfile.tar.bz2
+    [5]quite
     """)
     choice = int(input("Enter your choice: "))
     if choice == 1:
@@ -34,6 +40,8 @@ while True:
     elif choice == 3:
         zip() 
     elif choice == 4:
+        tar_bz2()
+    elif choice == 5:
         break
     else:
         print("invalid choice. Please try again.")
